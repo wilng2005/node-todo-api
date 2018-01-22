93eccd85d6a0de74f77217c2811790fcea071863
Wil Ng <wil.ng.2005@gmail.com>
2018-01-22 10:25:29 +0800

    First commit

---
+++ server/server.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+const port = process.env.PORT||3000;
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+	var todo = new Todo({
+		text:req.body.text
+	});
+
+	todo.save().then((doc)=>{
+		res.send(doc);
+	},(e)=>{
+		res.status(400).send(e);
+	});
+});
+
+app.get('/todos', (req,res)=>{
+	Todo.find().then((todos)=>{
+		res.send({
+			todos
+		});
+	},(e)=>{
+		res.status(400).send(e);
+	});
+});
+
+app.get('/todos/:id', (req,res)=>{
+	//res.send(req.params);
+	var id = req.params.id;
+
+	if(ObjectID.isValid(id)){
+		Todo.findById(id)
+		.then((todo)=>{
+			if(todo){
+				res.send({todo});
+			}else{
+				res.sendStatus(404);
+			}
+		}).catch((e)=>{
+			res.status(400).send();
+		});
+	}else{
+		res.sendStatus(404);
+	}
+});
+
+app.listen(port,()=>{
+	console.log(`Started on port ${port}`);
+});
+
+module.exports={app};

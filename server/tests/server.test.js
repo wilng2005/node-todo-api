6bbf91b70ac407690b67d88a8c54e5df64843780
Wil Ng <wil.ng.2005@gmail.com>
2018-02-14 09:41:12 +0800

    Implemented test for delete route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -99,4 +99,44 @@ describe('GET /todos/:id',()=>{
 		.expect(404)
 		.end(done);
 	});
-})
+});
+
+describe('DELETE /todos/:id',()=>{
+	it('should remove a todo',(done)=>{
+		
+		var hexId=todos[1]._id.toHexString();
+
+		request(app)
+		.delete(`/todos/${hexId}`)
+		.expect(200)
+		.expect((res)=>{
+			expect(res.body.todo.text).toBe(todos[1].text);
+		}).end((err,res)=>{
+			if(err){
+				return done(err)
+			}
+
+			Todo.findById(hexId)
+			.then((todo)=>{
+				expect(todo).toNotExist();
+				done();
+			})
+			.catch((e)=>done(e));
+		});
+	});
+
+	it('should return a 404 if todo not found',(done)=>{
+		var testId = new ObjectID();
+		request(app)
+		.delete(`/todos/${testId.toHexString()}`)
+		.expect(404)
+		.end(done);
+	});
+
+	it('should return a 404 for non-object ids',(done)=>{
+		request(app)
+		.delete(`/todos/3232`)
+		.expect(404)
+		.end(done);
+	});
+});

b688c2a50be19c7ca6def0e42f5f807c666308ec
Wil Ng <wil.ng.2005@gmail.com>
2018-02-19 12:31:23 +0800

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
@@ -51,6 +52,31 @@ app.get('/todos/:id', (req,res)=>{
 	}else{
 		res.sendStatus(404);
 	}
+});
+
+app.patch('/todos/:id',(req,res)=>{
+	var id = req.params.id;
+
+	var body = _.pick(req.body,['text','completed']);
+
+	if(_.isBoolean(body.completed)&&body.completed){
+		body.completedAt=new Date().getTime();
+	}else{
+		body.completed=false;
+		body.completedAt=null;
+	}
+
+	Todo.findByIdAndUpdate(id, {$set:body}, {new:true})
+	.then((todo)=>{
+		if(!todo){
+			res.status(404).send();
+		}else{
+			res.send(todo);	
+		}
+	})
+	.catch((e)=>{
+		res.status(400).send();
+	});
 });
 
 app.delete('/todos/:id',(req,res)=>{
